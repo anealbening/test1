a17fe156ddb13f6ce336262fc76fdfff07596e36
Aneal Bening <anealbening@hotmail.co.uk>
2022-07-06 13:41:35 +0100

    Inserted console log statement

--- src/index.js
+++ src/index.js
@@ -3,15 +3,5 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
-let counter = 1
-
-const refresh = () => {
-    ReactDOM.createRoot(document.getElementById('root')).render(<App counter={counter} />)
-}
-
-//setInterval() function used to refresh page in intervals so count increases
-setInterval(() => {
-    refresh()
-    counter += 1
-}, 1000)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
 
