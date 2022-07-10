b1e98bf55bcd2ca811e8f894f2eb7154f82f4037
Aneal Bening <anealbening@hotmail.co.uk>
2022-07-10 12:04:55 +0100

    Created Left and Right button

--- src/App.js
+++ src/App.js
@@ -1,44 +1,19 @@
 import { useState } from 'react'
 
-const Display = ({counter}) => {
-  return (
-    <div>{counter}</div>
-  )
-}
-
-const Button = ({onClick, text}) => {
-  return (
-    <button onClick = {onClick}>
-      {text}
-    </button>
-  )
-}
-
-
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
 
-  const increaseByOne = () => setCounter(counter + 1) //Increases counter value by 1
-  const decreaseByOne = () => setCounter(counter - 1) //Decreases counter value by 1
-
-  const setToZero = () => setCounter(0) //Sets counter value to zero
-
-
-  return (
+  return(
     <div>
-      <Display counter={counter}/>
-      <Button
-        onClick={increaseByOne}
-        text='plus'
-      />
-      <Button
-        onClick={setToZero}
-        text='zero'
-      />     
-      <Button
-        onClick={decreaseByOne}
-        text='minus'
-      />   
+      {left}
+      <button onClick={() => setLeft(left + 1)}>
+        left
+      </button>
+      <button onClick={() => setRight(right + 1)}>
+        right
+      </button>
+      {right}
     </div>
   )
 }
